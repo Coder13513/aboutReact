d28699456133117d248f311719e487906921badc
coder13513 <coder13513>
2020-12-28 20:03:43 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,233 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Navigate Between Screens using React Navigation in React Native //
+// https://aboutreact.com/react-native-stack-navigation //
+import 'react-native-gesture-handler';
 
-export default function App() {
+import * as React from 'react';
+import { Button, View, Text,  TouchableOpacity,  Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import splash from './pages/splash';
+import login from './pages/login';
+import register from './pages/register';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+// Import Custom Sidebar
+import CustomSidebarMenu from './pages/CustomSidebarMenu';
+import SplashScreen from './pages/SplashScreen'
+import DrawerNavigatorRoutes from './pages/DrawerNavigatorRoutes'
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const Auth = () => {
+  // Stack Navigator for Login and Sign up Screen
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen
+        name="LoginScreen"
+        component={login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="RegisterScreen"
+        component={register}
+        options={{
+          title: 'Register', //Set Header Title
+          headerStyle: {
+            backgroundColor: '#307ecc', //Set Header color
+          },
+          headerTintColor: '#fff', //Set Header text color
+          headerTitleStyle: {
+            fontWeight: 'bold', //Set Header text style
+          },
+        }}
+      />
+    </Stack.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        {/* SplashScreen which will come once for 5 Seconds */}
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          // Hiding header for Splash Screen
+          options={{headerShown: false}}
+        />
+        {/* Auth Navigator: Include Login and Signup */}
+        <Stack.Screen
+          name="Auth"
+          component={Auth}
+          options={{headerShown: true}}
+        />
+        {/* Navigation Drawer as a landing page */}
+         <Stack.Screen
+          name="DrawerNavigationRoutes"
+          component={DrawerNavigatorRoutes}
+          // Hiding header for Navigation Drawer
+          options={{headerShown: false}}
+        /> 
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
+// const NavigationDrawerStructure = (props)=> {
+//   //Structure for the navigatin Drawer
+//   const toggleDrawer = () => {
+//     //Props to open/close the drawer
+//     props.navigationProps.toggleDrawer();
+//   };
+
+//   return (
+//     <View style={{ flexDirection: 'row' }}>
+//       <TouchableOpacity onPress={()=> toggleDrawer()}>
+//         {/*Donute Button Image */}
+//         <Image
+//           source={{uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/drawerWhite.png'}}
+//           style={{
+//             width: 25,
+//             height: 25,
+//             marginLeft: 5
+//           }}
+//         />
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+// function firstScreenStack({ navigation }) {
+//   return (
+//       <Stack.Navigator initialRouteName="splash">
+//         <Stack.Screen
+//           name="splash"
+//           component={splash}
+//           options={{
+//             title: 'First Page Header', //Set Header Title
+//             headerLeft: ()=>
+//               <NavigationDrawerStructure
+//                 navigationProps={navigation}
+//               />,
+//             headerStyle: {
+//               backgroundColor: '#f4511e', //Set Header color
+//             },
+//             headerTintColor: '#fff', //Set Header text color
+//             headerTitleStyle: {
+//               fontWeight: 'bold', //Set Header text style
+//             },
+//           }}
+//         />
+//       </Stack.Navigator>
+//   );
+// }
+
+// function secondScreenStack({ navigation }) {
+//   return (
+//     <Stack.Navigator
+//       initialRouteName="login"
+//       screenOptions={{
+//         headerLeft: ()=>
+//           <NavigationDrawerStructure
+//             navigationProps={navigation}
+//           />,
+//         headerStyle: {
+//           backgroundColor: '#f4511e', //Set Header color
+//         },
+//         headerTintColor: '#fff', //Set Header text color
+//         headerTitleStyle: {
+//           fontWeight: 'bold', //Set Header text style
+//         }
+//       }}>
+//         <Stack.Screen
+//         name="register"
+//         component={register}
+//         options={{
+//           title: 'Third Page Header', //Set Header Title
+//         }}/>
+//       <Stack.Screen
+//         name="login"
+//         component={login}
+//         options={{
+//           title: 'Second Page Header', //Set Header Title
+          
+//         }}/>
+     
+//     </Stack.Navigator>
+//   );
+// }
+
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//     <Drawer.Navigator
+//       drawerContentOptions={{
+//         activeTintColor: '#e91e63',
+//         itemStyle: { marginVertical:5},
+//       }}
+//       drawerContent={(props) => <CustomSidebarMenu {...props} />}
+//       >
+//       <Drawer.Screen
+//         name="FirstPage"
+//         options={{ drawerLabel: 'First page Option' }}
+//         component={firstScreenStack} />
+//       <Drawer.Screen
+//         name="SecondPage"
+//         options={{ drawerLabel: 'Second page Option' }}
+//         component={secondScreenStack} />
+//     </Drawer.Navigator>
+//   </NavigationContainer>
+// -----------------------------------------------
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="splash">
+    //     <Stack.Screen
+    //       name="splash"
+    //       component={splash}
+    //       options={{
+    //         title: 'First Page', //Set Header Title
+    //         headerStyle: {
+    //           backgroundColor: '#f4511e', //Set Header color
+    //         },
+    //         headerTintColor: '#fff', //Set Header text color
+    //         headerTitleStyle: {
+    //           fontWeight: 'bold', //Set Header text style
+    //         },
+    //       }}
+    //     />
+    //     <Stack.Screen
+    //       name="login"
+    //       component={login}
+    //       options={{
+    //         title: 'Second Page', //Set Header Title
+    //         headerStyle: {
+    //           backgroundColor: '#f4511e', //Set Header color
+    //         },
+    //         headerTintColor: '#fff', //Set Header text color
+    //         headerTitleStyle: {
+    //           fontWeight: 'bold', //Set Header text style
+    //         },
+    //       }}
+    //     />
+    //     <Stack.Screen
+    //       name="register"
+    //       component={register}
+    //       options={{
+    //         title: 'Third Page', //Set Header Title
+    //         headerStyle: {
+    //           backgroundColor: '#f4511e', //Set Header color
+    //         },
+    //         headerTintColor: '#fff', //Set Header text color
+    //         headerTitleStyle: {
+    //           fontWeight: 'bold', //Set Header text style
+    //         },
+    //       }}
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+//   );
+// }
+
+export default App;
---
+++ pages/login.js
@@ -0,0 +1,382 @@
+// Example of Splash, Login and Sign Up in React Native
+// https://aboutreact.com/react-native-login-and-signup/
+
+// Import React and Component
+import React, {useState, createRef} from 'react';
+import {
+  Alert,
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+// import Loader from './Components/Loader';
+
+const LoginScreen = ({navigation}) => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  // const [loading, setLoading] = useState(false);
+  const [errortext, setErrortext] = useState('');
+
+  const passwordInputRef = createRef();
+   
+
+
+ const sendMessage = () => {
+    Alert.alert(userEmail, userPassword);
+    navigation.replace('DrawerNavigationRoutes');
+    // this.props.navigation.navigate('HomeScreenRoute');
+    // this.props.navigation.goBack();
+}
+  const handleSubmitPress = () => {
+    setErrortext('');
+    if (!userEmail) {
+      alert('Please fill Email');
+      return;
+    }
+    if (!userPassword) {
+      alert('Please fill Password');
+      return;
+    }
+    // setLoading(true);
+    let dataToSend = {
+          email: userEmail,
+          password: userPassword
+        };
+    let formBody = [];
+    for (let key in dataToSend) {
+      let encodedKey = encodeURIComponent(key);
+      let encodedValue = encodeURIComponent(dataToSend[key]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    console.log(formBody),
+    
+    formBody = formBody.join('&');
+
+    console.log(formBody),
+    
+
+    fetch('http://127.0.0.1:8000/auth/login/', {
+      method: 'POST',
+   
+      body: formBody,
+
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type':'application/json'
+    }
+      
+      // headers: {
+      //   //Header Defination
+      //   'Accept': 'application/json, text/plain',
+      //   'Content-Type':  'application/json'
+      // },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        //Hide Loader
+        // setLoading(false);
+        console.log(responseJson);
+        // If server response message same as Data Matched
+        if (responseJson.status == 1) {
+          AsyncStorage.setItem(
+            'user_id',
+             responseJson.data[0].user_id
+          );
+          console.log(responseJson.data[0].user_id);
+          navigation.replace('DrawerNavigationRoutes');
+        } else {
+          setErrortext('Please check your email id or password');
+          console.log('Please check your email id or password');
+        }
+      })
+      .catch((error) => {
+        //Hide Loader
+        // setLoading(false);
+        console.error(error);
+      });
+  };
+
+  return (
+    <View style={styles.mainBody}>
+      {/* <Loader loading={loading} /> */}
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}>
+        <View>
+          <KeyboardAvoidingView enabled>
+            <View style={{alignItems: 'center'}}>
+              <Image
+                source={require('../assets/thumbnail_Leanvia-logo.png')}
+                style={{
+                  width: '50%',
+                  height: 100,
+                  resizeMode: 'contain',
+                  margin: 40,
+                  // marginTop:-10
+                }}
+              />
+              <Text style={styles.heading} > Hospital Management System</Text> 
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={(UserEmail) => setUserEmail(UserEmail)}
+                placeholder="Enter Email" //dummy@abc.com
+                placeholderTextColor="#8b9cb5"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordInputRef.current &&
+                  passwordInputRef.current.focus()
+                }
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={
+                  (UserPassword) => setUserPassword(UserPassword)
+                }
+                placeholder="Enter Password" //12345
+                placeholderTextColor="#8b9cb5"
+                keyboardType="default"
+                ref={passwordInputRef}
+                onSubmitEditing={Keyboard.dismiss}
+                blurOnSubmit={false}
+                secureTextEntry={true}
+                underlineColorAndroid="#f000"
+                returnKeyType="next"
+              />
+            </View>
+            {errortext != '' ? (
+              <Text style={styles.errorTextStyle}>
+                {errortext}
+              </Text>
+            ) : null}
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              activeOpacity={0.5}
+              onPress={sendMessage}>
+              <Text style={styles.buttonTextStyle}>
+                LOGIN
+              </Text>
+            </TouchableOpacity>
+            <Text
+              style={styles.registerTextStyle}
+              onPress={() => navigation.navigate('RegisterScreen')}>
+              New Here ? Register
+            </Text>
+          </KeyboardAvoidingView>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#307ecc',
+    // backgroundColor: 'white',
+    alignContent: 'center',
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#7DE24E',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: 'white',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: '#dadae8',
+  },
+  registerTextStyle: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
+  heading:{
+    // borderBottomWidth:2,
+    borderColor:'black',
+    fontSize:20,
+    // marginBottom:20,
+    marginTop:-70
+    // backgroundColor:'red'
+},
+});
+// --------------------------old login-------------------
+// Navigate Between Screens using React Navigation in React Native //
+// https://aboutreact.com/react-native-stack-navigation //
+// import * as React from 'react';
+// import { Button, View, Text, SafeAreaView,Image ,StyleSheet} from 'react-native';
+
+// const login = ({ navigation }) => {
+//   return (
+//     <SafeAreaView style={{ flex: 8}}>
+//       <View style={{ flex: 3, padding: 16 ,
+//         backgroundColor:'yellow'
+//       }}
+//         >
+//         <View
+//           style={{
+//            flex:3,
+//             alignItems: 'center',
+//             justifyContent: 'center',
+//             backgroundColor:'red'
+//           }}>
+            
+//              <Image
+//         source={require('../assets/thumbnail_Leanvia-logo.png')}
+//         style={{width: '30%',height:100, resizeMode: 'contain', 
+//         margin: 10
+//       }}
+//       />
+//       <Text style={styles.heading} > Hospital Management System</Text> 
+//           {/* <Text
+//             style={{
+//               fontSize: 25,
+//               textAlign: 'center',
+//               marginBottom: 16
+//             }}>
+//             This is Second Page of the App
+//           </Text>  */}
+//           <Button
+//             title="Go back"
+//             onPress={() => navigation.goBack()}
+//           />
+//           <Button
+//             title="Go to SecondPage... again"
+//             onPress={() => navigation.push('login')}
+//           />
+//           <Button
+//             title="Replace this screen with Third Page"
+//             onPress={() =>
+//               navigation.replace('register', {
+//                 someParam: 'Param',
+//               })
+//             }
+//           /> 
+//           {/* <Button
+//             title="Reset navigator state to Third Page"
+//             onPress={() =>
+//               navigation.reset({
+//                 index: 0,
+//                 routes: [
+//                   {
+//                     name: 'register',
+//                     params: { someParam: 'reset' },
+//                   },
+//                 ],
+//               })
+//             }
+//           />
+//           <Button
+//             title="Go to First Page"
+//             onPress={() => navigation.navigate('splash')}
+//           /> */}
+//       <Button
+//             title="Go to Third Page"
+//             onPress={() =>
+//               navigation.navigate(
+//                 'register', { someParam: 'Param1' }
+//               )
+//             }
+//           />
+//         </View>
+//         {/* <Text
+//           style={{
+//             fontSize: 18,
+//             textAlign: 'center',
+//             color: 'grey'
+//           }}>
+//           Navigate Between Screens using
+//           {'\n'}
+//           React Navigation
+//         </Text> */}
+//         {/* <Text
+//           style={{
+//             fontSize: 16,
+//             textAlign: 'center',
+//             color: 'grey'
+//           }}>
+//           www.aboutreact.com
+//         </Text> */}
+//         {/* </View> */}
+//       </View>
+//     </SafeAreaView>
+//   );
+// }
+
+// export default login;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: 'white',
+//   },
+//   activityIndicator: {
+//     alignItems: 'center',
+//     height: 80,
+//   },
+//   heading:{
+//     // borderBottomWidth:2,
+//     borderColor:'black',
+//     fontSize:15,
+//     // marginBottom:20,
+//     marginTop:-50
+//     // backgroundColor:'red'
+// },
+// });
